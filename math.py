6ad77c6adcd96e537a31d0eca9c451979ee6ea71
AbhisekMishra <abhisekmishra150@gmail.com>
2022-02-21 16:33:05 +0530

    Added function headers

--- math.py
+++ math.py
@@ -1,13 +1,16 @@
 
 from distutils.log import error
 
-
+#addition implementation
 def add(x,y):
     pass
+#substraction implementation
 def substract(x,y):
     pass
+#multiplication implementation
 def multiply(x,y):
     pass
+#division implementation
 def divide(x,y):
     pass
         
